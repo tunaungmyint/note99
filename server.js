9394adeebeb7f583f4d9e4dec896781a6d72e915
Tun Aung Myint <kakyi99999@gmail.com>
2022-12-01 17:58:06 +0630

    add middleware

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './frontend/build')));
 
 // connect to mongoose
 mongoose.connect(process.env.MONGO_URI, () =>
